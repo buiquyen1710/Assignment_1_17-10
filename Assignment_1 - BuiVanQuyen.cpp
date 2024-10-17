46f189f7c41977d62bbf0a201b220b985316c336
buiquyen1710 <165695976+buiquyen1710@users.noreply.github.com>
2024-10-17 16:02:13 +0700

    Add files via upload

---
+++ Assignment_1 - BuiVanQuyen.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+using namespace std;
+void sapxep(int a[], int n) {
+	for (int i = 0; i < n-1; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (a[j] < a[i]) {
+				int temp = a[j];
+				a[j] = a[i];
+				a[i] = temp;
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		if (a[i] == 0) {
+			cout << "RED" << " ";
+		}
+		else if(a[i]==1) {
+			cout << "WHITE" << " ";
+		}
+		else if (a[i] == 2) {
+			cout << "BLUE" << " ";
+		}
+	}
+}
+int main() {
+	int n;
+	cin >> n;
+	int* a = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	sapxep(a, n);
+	return 0;
+}
